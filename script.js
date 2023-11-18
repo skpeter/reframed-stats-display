375379f2f3feef6c1d935a6a70266d288053655e
Samuel Petersen <samuelkpeter@gmail.com>
2023-11-18 01:44:18 -0300

    pass 1

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ws = new WebSocket(reframed_url); // Replace with your WebSocket URL
     let currentStatIndex = 0;
+    statsData = { names: [], values: [] }; // To store the latest stats data
+
+    const updateDisplay = () => {
+        if (currentStatIndex >= statsData.names.length) {
+            currentStatIndex = 0;
+        }
+
+        if (statsData.names.length > 0 && statsData.values.length > 0) {
+            document.getElementById('player1').textContent = statsData.values[currentStatIndex][0];
+            document.getElementById('stat-name').textContent = statsData.names[currentStatIndex];
+            document.getElementById('player2').textContent = statsData.values[currentStatIndex][1];
+        }
+
+        currentStatIndex = (currentStatIndex + 1) % statsData.names.length;
+    };
 
     ws.onmessage = function(event) {
         const reader = new FileReader();
@@ -14,18 +29,8 @@ document.addEventListener('DOMContentLoaded', () => {
                     return;
                 }
 
-                const { names, values } = data.stats;
-
-                // Update the display
-                if (currentStatIndex >= names.length) {
-                    currentStatIndex = 0;
-                }
-
-                document.getElementById('player1').textContent = values[currentStatIndex][0];
-                document.getElementById('stat-name').textContent = names[currentStatIndex];
-                document.getElementById('player2').textContent = values[currentStatIndex][1];
-
-                currentStatIndex = (currentStatIndex + 1) % names.length;
+                // Update statsData with the latest data
+                statsData = data.stats;
             } catch (error) {
                 console.error('Error parsing JSON:', error);
             }
@@ -41,4 +46,7 @@ document.addEventListener('DOMContentLoaded', () => {
     ws.onclose = function() {
         console.log('WebSocket connection closed');
     };
+
+    // Set interval to update the display every 10 seconds
+    setInterval(updateDisplay, 10000);
 });
