9b7f2141c89d9da77d08b897e5f3e605393c716f
Samuel Petersen <samuelkpeter@gmail.com>
2023-11-18 23:33:53 -0300

    adjustments

--- config-example.js
+++ config-example.js
@@ -1,1 +1,3 @@
 const reframed_url = 'ws://127.0.0.1:1337'
+const suffix_text = 'Stats compiled by ReFramed (@TheComet92 & @Leeevye)'
+const remove_complimentary_data = false 
--- script.js
+++ script.js
@@ -1,26 +1,55 @@
 document.addEventListener('DOMContentLoaded', () => {
     const ws = new WebSocket(reframed_url); // Replace with your WebSocket URL
+    document.getElementById("copyright").innerText = suffix_text
     let currentStatIndex = 0;
     statsData = { names: [], values: [] }; // To store the latest stats data
 
     const updateDisplay = () => {
-        if (currentStatIndex >= statsData.names.length) {
-            currentStatIndex = 0;
-        }
+        document.getElementsByClassName("box")[0].classList.add('show')
+        const elements = [document.getElementById('player1'), document.getElementById('stat-name'), document.getElementById('player2')];
+        elements.forEach(el => el.classList.add('fade-animation'));
 
-        if (statsData.names.length > 0 && statsData.values.length > 0) {
-            document.getElementById('player1').textContent = statsData.values[currentStatIndex][0];
-            document.getElementById('stat-name').textContent = statsData.names[currentStatIndex];
-            document.getElementById('player2').textContent = statsData.values[currentStatIndex][1];
-        }
+        setTimeout(() => {
+            if (currentStatIndex >= statsData.names.length) {
+                currentStatIndex = 0;
+            }
 
-        currentStatIndex = (currentStatIndex + 1) % statsData.names.length;
+            try {
+                if (statsData.names.length > 0 && statsData.values.length > 0) {
+                    if (remove_complimentary_data) {
+                        if (statsData.values[currentStatIndex][0].split("/").length > 1) {
+                            statsData.values[currentStatIndex][0] = statsData.values[currentStatIndex][0].split("/")[1]
+                            statsData.values[currentStatIndex][1] = statsData.values[currentStatIndex][1].split("/")[1]
+                        }
+                    }
+                    document.getElementById('player1').textContent = statsData.values[currentStatIndex][0];
+                    document.getElementById('stat-name').textContent = statsData.names[currentStatIndex];
+                    document.getElementById('player2').textContent = statsData.values[currentStatIndex][1];
+                    currentStatIndex = (currentStatIndex + 1) % statsData.names.length;
+                }
+            } catch {
+                currentStatIndex = 0;
+            }
+
+
+        }, 1000)
+
+        // Remove the fade animation class after the animation ends
+        elements.forEach(el => {
+            el.addEventListener('animationend', () => {
+                el.classList.remove('fade-animation');
+            }, { once: true });
+        });
+
+        if (statsData.values.ended) setTimeout(() => {
+            document.getElementsByClassName("box")[0].classList.remove('show')
+        }, 90000)
     };
 
-    ws.onmessage = function(event) {
+    ws.onmessage = function (event) {
         const reader = new FileReader();
 
-        reader.onload = function() {
+        reader.onload = function () {
             try {
                 const data = JSON.parse(reader.result);
 
@@ -37,13 +66,14 @@ document.addEventListener('DOMContentLoaded', () => {
         };
 
         reader.readAsText(event.data);
+
     };
 
-    ws.onerror = function(error) {
+    ws.onerror = function (error) {
         console.error('WebSocket Error:', error);
     };
 
-    ws.onclose = function() {
+    ws.onclose = function () {
         console.log('WebSocket connection closed');
     };
 
