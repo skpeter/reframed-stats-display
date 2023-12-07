a12936fec603f9d11fe9e3611155505df0320538
Samuel Petersen <samuelkpeter@gmail.com>
2023-12-07 13:19:38 -0300

    update example config

--- config-example.js
+++ config-example.js
@@ -1,4 +1,4 @@
 const reframed_url = 'ws://127.0.0.1:1337'
 const suffix_text = 'Stats compiled by ReFramed (@TheComet92 & @Leeevye)'
-const include_stats = [] // leave as empty array [] to include all
+const include_stats = ['Stage Control', 'Openings / Kill', 'Earliest Death', 'Total Damage Dealt', 'Neutral Wins', 'Stocks'] // leave as empty array [] to include all
 const remove_complimentary_data = false //remove complimentary data from some stats
